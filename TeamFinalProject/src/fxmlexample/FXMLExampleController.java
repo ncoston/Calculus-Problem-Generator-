0c2e6210ccaa0a5e51d2b7c3eb29e882c888af3c
Katharine Adamyk <katharine.adamyk@colorado.edu>
2016-08-13 17:12:10 -0600

    Submit button gets and prints XML from Wolfram

--- TeamFinalProject/src/fxmlexample/FXMLExampleController.java
+++ TeamFinalProject/src/fxmlexample/FXMLExampleController.java
@@ -9,8 +9,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.text.Text;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
@@ -23,39 +21,49 @@ import javax.ws.rs.core.Response;
 
 
 /*This files handles the mouse and keyboard input.
-
 */
 
-//Our AppID is: GLHTK4-96L2PVQT4T
 public class FXMLExampleController {
+    /**We'll need our appId, client, and target each time we call Wolfram Alpha
+     * To call Wolfram Alpha the URL needs to be extended to include the input 
+     * and appid. This can be done using .queryParam("name of parameter", value).
+     */
+    String appId = "GLHTK4-96L2PVQT4T";
+    Client client = ClientBuilder.newClient();
+    WebTarget wolframTarget = client.target("http://api.wolframalpha.com/v2/query");
     
-    
-    
+    //Here are the GUI elements we want to interact with in various methods:
     @FXML private Label actiontarget;
-    
     @FXML private TextArea textEntered;
     
     
+    /** The sendText method sends entered text to Wolfram Alpha when the "Submit"
+    * button is pressed and then displays the output. (So far it can print out 
+    * the XML but eventually it will print something nicer).
+    */
     @FXML protected void sendText(ActionEvent event) {
-        String appId = "GLHTK4-96L2PVQT4T";
-        String input = "Nothing entered";
-        Client client = ClientBuilder.newClient();
-        WebTarget wolframTarget = client.target("http://api.wolframalpha.com/v2/query");
-        //When the Client instance is uncommented the code will run, but produce an exception if this line is called.
-        //WebTarget resourceWebTarget = client.target(path);
-        //Something about how WebTaget.path is non-static and can't be referenced from a static context?
+        
+        //If no text is entered, nothing happens - we do not send WA the empty text
         if (textEntered.getText() != null) {
-            input = textEntered.getText();
+            
+            //First we get the text that has been entered in the text area so far
+            String input = textEntered.getText();
+            
+            /**This adds ?input=<modified value of input>&appid=<value of appId>
+             * to the URL so that WA can be called correctly. queryParam
+             * modifies the string in input into appropriate syntax for the 
+             * query URL.
+             */
             WebTarget queryTarget = wolframTarget.queryParam("input", input).queryParam("appid", appId);
+            
+            //Here we call WA and read their response
             Response response = queryTarget.request().get();
-            actiontarget.setText(response.toString());
-            System.out.println(response);
+            String responseText = response.readEntity(String.class);
+          
+            //For now, we'll display the xml in the GUI and print it to the program output
+            actiontarget.setText(responseText);
+            System.out.println(responseText);
         }
-            
-            
-        }
-        
-        
-        
-    }
+    }   
+}
 
